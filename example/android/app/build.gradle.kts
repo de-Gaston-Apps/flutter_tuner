ac987d5e8281a6cba860087504196f0dff652634
Jonathan de Gaston <developerjon5@gmail.com>
2025-10-19 00:53:05 -0600

    Fix all the 16 KB stuff for once and for all

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.degastonapps.flutter_tuner_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "29.0.13113456 rc1"
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
